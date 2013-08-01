45ba67682fc76e6bf532fa0bd64004022eb47b3c
Mutassem Abu rijea <mu3tsm@gmail.com>
2013-08-01 20:41:29 +0000

    modify web.js

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
+var fs = require('fs')
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var html = fs.readFile('./index.html', function(err, data){
+    if (err) throw err;
+    console.log(data);
 });
 
-var port = process.env.PORT || 5000;
+app.get('/', function(request, response) {
+    response.sendfile('./index.html');
+});
+
+var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
