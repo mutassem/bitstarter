f0761d0d6f066ba8b9a67b8f9a29d108ccde76c1
Mutassem Abu rijea <mu3tsm@gmail.com>
2013-08-01 23:38:43 +0000

    fix port on web.js to 5000

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 var fs = require('fs')
 app.use(express.logger());
 
-var html = fs.readFile('./index.html', function(err, data){
+fs.readFile('./index.html', function(err, data){
     if (err) throw err;
     console.log(data);
 });
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
     response.sendfile('./index.html');
 });
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
